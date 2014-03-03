3a43cbf9f655b2526d8ff73adacda4ff78f1aaad
chad <chad-dev@katica.org>
2014-03-03 17:24:58 +0000

    ankisim - small improvements

--- ankisim/sample_04.py
+++ ankisim/sample_04.py
@@ -3,7 +3,7 @@ from ankisim import Card, Deck
 print """
 Simulation #4
 -------------
-Simulate adding 30 new cards every day fro a 3000 item list, to
+Simulate adding 30 new cards every day from a 3000 item list, to
 see how the queue progresses. This simulates cramming of 3000
 characters by learning 30 new ones a day.
 """
@@ -12,6 +12,10 @@ numToLearn = 3000
 
 deck = Deck()
 deck.maxPerDay = 200
+
+type = 'SIMULATED'
+print "Card Type: %s" % type
+print "Max cards per day: %d\n" % deck.maxPerDay
 
 for day in xrange(200):
 	for x in xrange( min(30, numToLearn - len(deck.cards()) )  ):
